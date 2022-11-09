10f2b1da3ce504b33968f300b331b0efd20eb66d
Cube X <shawn6304@qq.com>
2022-11-09 11:53:48 +0800

    s

--- 学习通网课助手.js
+++ 学习通网课助手.js
@@ -100,6 +100,7 @@
 
     //直播回放
     if (document.URL.includes('zhibo.chaoxing.com') && !document.URL.includes('iframe')) {
+        console.clear();
         console.log('脚本加载完成');
         let n = 0;
         setInterval(() => {
